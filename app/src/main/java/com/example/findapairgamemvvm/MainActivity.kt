90310806aec12439b879fb6dd52363e985264a0a
NumpyGlumpy <khaypov03@mail.ru>
2023-05-10 18:31:57 +0300

    - 000 app created

---
+++ app/src/main/java/com/example/findapairgamemvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.findapairgamemvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
